87dd11bc85b84788c619fe95623c99bc5de91ca7
nelsonleopold <nelsonleopoldiv@guamcc.edu>
2020-09-18 11:04:11 +1000

    new python homework

---
+++ NLeopold_while_blastoff.py
@@ -0,0 +1,9 @@
+#Name: Nelson Leopold
+#Date: 9/18/2020
+#Describe what the program will do -- This program counts down from 10, then outputs Blastoff!!
+
+count = 10
+while count > 0:
+    print(count)
+    count -= 1
+print("Blastoff!!")
---
+++ NLeopold_while_sumof1to10.py
@@ -0,0 +1,11 @@
+#Name: Nelson Leopold
+#Date: 9/18/2020
+#Describe what the program will do -- This progam will sum the numbers from 1 to 10 and ouput the result
+
+sum = 0
+lastNum = 10
+num = 1
+while num <= lastNum:
+    sum += num
+    num += 1
+print(sum)
